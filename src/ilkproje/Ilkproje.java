2e2295ae60808c6e6d12275fa502deb332ed0d3d
ttaha <mustafatahacilbas@gmail.com>
2021-09-28 13:44:57 +0300

    Initial commit

---
+++ src/ilkproje/Ilkproje.java
@@ -0,0 +1,16 @@
+package ilkproje;
+
+public class Ilkproje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //sysout ctrl space
+		System.out.println("bane q.");
+		System.out.println("Emrah");
+		System.out.println(" ");
+		System.out.println("Bayram");//println bayram
+		System.out.println("Muzaffer"+" "+"Övün");
+		System.out.println("Yaþ "+38);
+	}
+
+}
